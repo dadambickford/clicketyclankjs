796115fdde1d5499a3467c5bb163f447f4b9eff5
Adam Bickford <xjunkyardx@gmail.com>
2014-10-12 23:09:54 -0700

    I think gh-pages is just taking too long

--- tdfw.js
+++ tdfw.js
@@ -105,7 +105,7 @@
 			removeCurStyles()
 		}
 	}
-	//<iframe width="560" height="315" src="//www.youtube.com/embed/HMUDVMiITOU?autoplay=1" frameborder="0" allowfullscreen></iframe>
+	//<iframe width="560" height="315" src="//www.youtube.com/embed/Xz8k72nRjMk?autoplay=1" frameborder="0" allowfullscreen></iframe>
 	
 	function setupAnimations() {
 		var numKeyFrames = 10
