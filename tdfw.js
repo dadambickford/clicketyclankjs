30d52ef577e4ef026a5298eefc02de4578f4c031
Adam Bickford <xjunkyardx@gmail.com>
2014-10-12 22:46:23 -0700

    swap video id

--- tdfw.js
+++ tdfw.js
@@ -67,7 +67,7 @@
 			player = new YT.Player('tdfw', {
 				height: '200',
 				width: '305',
-				videoId: 'HMUDVMiITOU',
+				videoId: 'Xz8k72nRjMk',
 				events: {
 					'onReady': onPlayerReady,
 					'onStateChange': onPlayerStateChange
